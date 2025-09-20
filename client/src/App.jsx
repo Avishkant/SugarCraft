366898ba4d2876918c86acd472d582873316a952
Avishkant <avishkantpatidar22@gmail.com>
2025-09-20 21:29:10 +0530

    Resolving hosting issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
 
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import AdminDashboard from './pages/AdminDashboard';
-import CustomerDashboard from './pages/CustomerDashboard';
-import Sweets from './pages/Sweets';
-import About from './pages/About';
-import Cart from "./pages/Cart.jsx";
+  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  import Home from './pages/Home';
+  import Login from './pages/Login';
+  import Register from './pages/Register';
+  import AdminDashboard from './pages/AdminDashboard';
+  import CustomerDashboard from './pages/CustomerDashboard';
+  import Sweets from './pages/Sweets';
+  import About from './pages/About';
+  import Cart from './pages/Cart.jsx';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/dashboard" element={<CustomerDashboard />} />
-      <Route path="/sweets" element={<Sweets />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  );
-}
 
-export default App;
+  function App() {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/dashboard" element={<CustomerDashboard />} />
+        <Route path="/sweets" element={<Sweets />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    );
+  }
+
+  export default App;
